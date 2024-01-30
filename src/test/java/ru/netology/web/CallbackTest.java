53615c69d1107f13d8f9db98b20709f6227b8682
PROGRAMMER <motafaka@yandex.ru>
2024-01-30 18:53:53 +0300

    settings.gitignore

--- src/test/java/ru/netology/web/CallbackTest.java
+++ src/test/java/ru/netology/web/CallbackTest.java
@@ -3,22 +3,23 @@ package ru.netology.web;
 import com.codeborne.selenide.Condition;
 import com.codeborne.selenide.SelenideElement;
 import org.junit.jupiter.api.Test;
+
 import static com.codeborne.selenide.Selenide.$;
 import static com.codeborne.selenide.Selenide.open;
 
 
 public class CallbackTest {
 
-@Test
+    @Test
     void shouldTestSomething() {
-       open("http://localhost:9999");
+        open("http://localhost:9999");
 
-SelenideElement form = $(".form_theme_alfa-on-white");
-form.$("[data-test-id=name] input") .setValue("Иван Пучков");
-form.$("[data-test-id=phone] input") .setValue("+79000000000");
-form.$("[data-test-id=agreement]") .click();
-form.$(".button_theme_alfa-on-white") .click();
-$("[data-test-id=order-success]").shouldHave(Condition.exactText("Ваша заявка успешно отправлена! Наш менеджер свяжется с вами в ближайшее время."));
+        SelenideElement form = $(".form_theme_alfa-on-white");
+        form.$("[data-test-id=name] input").setValue("Иван Пучков");
+        form.$("[data-test-id=phone] input").setValue("+79000000000");
+        form.$("[data-test-id=agreement]").click();
+        form.$(".button_theme_alfa-on-white").click();
+        $("[data-test-id=order-success]").shouldHave(Condition.exactText("Ваша заявка успешно отправлена! Наш менеджер свяжется с вами в ближайшее время."));
 
-}
+    }
 }
